1f62457be6386b636351c90c822ae98aa041691a
urionz <urionz@urionz.com>
2021-03-04 10:50:25 +0800

    add config external method

--- config/config.go
+++ config/config.go
@@ -6,11 +6,17 @@ import (
 )
 
 type IConfig interface {
+	Get(key string, findByPath ...bool) interface{}
 	Set(key string, val interface{}, setByPath ...bool) error
 	String(key string, defVal ...string) string
+	Strings(key string) (arr []string)
 	Int(key string, defVal ...int) int
+	Ints(key string) (arr []int)
+	Int64(key string, defVal ...int64) (value int64)
+	Uint(key string, defVal ...uint) (value uint)
 	Bool(key string, defVal ...bool) bool
 	Env(key string, defVal interface{}) interface{}
+	Exists(key string, findByPath ...bool) bool
 	LoadExists(...string) error
 	Object(key string, findByPath ...bool) IConfig
 	Data() map[string]interface{}
@@ -53,6 +59,14 @@ func LoadExists(files ...string) error {
 	return serve.LoadExists(files...)
 }
 
+func Get(key string, findByPath ...bool) interface{} {
+	return serve.Get(key, findByPath...)
+}
+
+func Exists(key string, findByPath ...bool) bool {
+	return serve.Exists(key, findByPath...)
+}
+
 func Env(key string, defVal interface{}) interface{} {
 	return serve.Env(key, defVal)
 }
@@ -65,10 +79,26 @@ func String(key string, defVal ...string) string {
 	return serve.String(key, defVal...)
 }
 
-func Int(key string, defVal ...int) (value int) {
+func Strings(key string) []string {
+	return serve.Strings(key)
+}
+
+func Int(key string, defVal ...int) int {
 	return serve.Int(key, defVal...)
 }
 
-func Bool(key string, defVal ...bool) (value bool) {
+func Ints(key string) []int {
+	return serve.Ints(key)
+}
+
+func Int64(key string, defVal ...int64) int64 {
+	return serve.Int64(key, defVal...)
+}
+
+func Uint(key string, defVal ...uint) uint {
+	return serve.Uint(key, defVal...)
+}
+
+func Bool(key string, defVal ...bool) bool {
 	return serve.Bool(key, defVal...)
 }
--- config/service_provider_test.go
+++ config/service_provider_test.go
@@ -20,6 +20,12 @@ func TestNewConfigServiceProvider(t *testing.T) {
 			require.Equal(t, true, config.Env("bool", true))
 			require.Equal(t, "redis", config.Object("cache.stores").String("redis.driver"))
 			require.Equal(t, map[string]interface{}{}, config.Object("no.found").Data())
+			require.Equal(t, []int{1, 2, 3}, config.Ints("app.ints"))
+			require.Equal(t, []string{"1", "2", "3"}, config.Strings("app.strings"))
+			require.Equal(t, "test", config.Get("app.test"))
+			require.Equal(t, false, config.Exists("a.b.c.d"))
+			require.Equal(t, uint(1), config.Uint("app.uint"))
+			require.Equal(t, int64(1), config.Int64("app.uint"))
 		}).Run()
 	})
 }
